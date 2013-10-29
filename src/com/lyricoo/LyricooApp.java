e0d6e2f14f97f86f128b728650da2900681266d9
Eli Hart <elihart@stanford.edu>
2013-10-28 15:53:38 -1000

    Create basic activities and extend application class

---
+++ src/com/lyricoo/LyricooApp.java
@@ -0,0 +1,21 @@
+package com.lyricoo;
+
+import android.app.Application;
+
+/*
+ * The LyricooApp class provides a global storage area for
+ * data that's shared between activities. We can store information
+ * about the logged in user and their session.
+ */
+
+public class LyricooApp extends Application {
+	@Override
+	  public void onCreate()
+	  {
+	    super.onCreate();
+	    
+	    // Initialize any singletons or variables here
+	    
+	  }
+
+}
---
+++ src/com/lyricoo/activity/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.lyricoo.activity;
+
+import com.lyricoo.R;
+import com.lyricoo.R.layout;
+import com.lyricoo.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+}
