342e6590a9e3c7efca1ab70440454167fcfa6127
Calvin Fernandez <cfernand@cs.stanford.edu>
2014-01-03 15:34:43 -0800

    moved pulling actions to friend fragment

--- src/com/lyricoo/friends/FriendsActivity.java
+++ src/com/lyricoo/friends/FriendsActivity.java
@@ -125,88 +125,10 @@ public class FriendsActivity extends LyricooActivity {
 		return true;
 	}
 
-	// shows options for interacting with the given friend
-	// in a dialog popup
-	private void showOptions(final User friend) {
-		// the options to show in the dialog list
-		String[] options = { "Delete" };
-		// create a new dialog
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		// set the title to be the username of the friend
-		builder.setTitle(friend.getUsername())
-		// add the options to the list
-				.setItems(options, new DialogInterface.OnClickListener() {
-					// handle clicking on an option
-					public void onClick(DialogInterface dialog, int which) {
-						switch (which) {
-						// "Delete" is the first option in the array
-						case 0:
-							Session.getFriendManager().removeFriend(friend);
-							break;
-						default:
-							break;
-						}
-					}
-				});
-		builder.create().show();
-	}
-
 	// Start an activity where the user can add friends to their list
 	public void addFriendClicked(View v) {
 		Intent i = new Intent(this, ContactsActivity.class);
 		startActivity(i);
-	}
-
-	// Load the conversation with this friend
-	private void loadConversation(User friend) {
-		// Pass the friend so conversationActivity knows whose conversation to
-		// display.
-		// Convert to json to make it easy to pass to the object
-		String friendAsJson = Utility.toJson(friend);
-
-		Intent i = new Intent(mContext, ConversationActivity.class);
-		i.putExtra("contact", friendAsJson);
-		startActivity(i);
-	}
-
-	private void loadFriendsList() {
-
-		mFriends = Session.getFriendManager().getFriends();
-
-		// Create adapter for the list view
-		FriendsListAdapter adapter = new FriendsListAdapter(mContext,
-				R.layout.friend_list_item, mFriends);
-
-		mList.setAdapter(adapter);
-
-		// on long click show a list of options to the user
-		mList.setOnItemLongClickListener(new OnItemLongClickListener() {
-
-			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				User friend = mFriends.get(position);
-				showOptions(friend);
-
-				// Return true to indicate that we have handled the
-				// click
-				return true;
-			}
-		});
-
-		// on normal click take the user to the conversation with this
-		// friend
-		mList.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				User friend = mFriends.get(position);
-				loadConversation(friend);
-			}
-
-		});
-
 	}
 
 	/**
--- src/com/lyricoo/friends/FriendsFragment.java
+++ src/com/lyricoo/friends/FriendsFragment.java
@@ -8,12 +8,16 @@ import com.lyricoo.LyricooActivity;
 import com.lyricoo.R;
 import com.lyricoo.Utility;
 import com.lyricoo.api.LyricooApiResponseHandler;
+import com.lyricoo.messages.ConversationActivity;
 import com.lyricoo.session.Session;
 import com.lyricoo.session.User;
 import com.lyricoo.ui.SlidingMenuHelper;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -22,6 +26,8 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
+
+import android.app.AlertDialog;
 
 public class FriendsFragment extends Fragment {
 	private ArrayList<User> mFriends;
@@ -45,7 +51,7 @@ public class FriendsFragment extends Fragment {
 			public boolean onItemLongClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				User friend = mFriends.get(position);
-				//showOptions(friend);
+				showOptions(friend);
 
 				// Return true to indicate that we have handled the
 				// click
@@ -61,11 +67,49 @@ public class FriendsFragment extends Fragment {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				User friend = mFriends.get(position);
-				//loadConversation(friend);
+				loadConversation(friend);
 			}
 
 		});
 
+	}
+	
+	// shows options for interacting with the given friend
+	// in a dialog popup
+	private void showOptions(final User friend) {
+		// the options to show in the dialog list
+		String[] options = { "Delete" };
+		// create a new dialog
+		AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+		// set the title to be the username of the friend
+		builder.setTitle(friend.getUsername())
+		// add the options to the list
+				.setItems(options, new DialogInterface.OnClickListener() {
+					// handle clicking on an option
+					public void onClick(DialogInterface dialog, int which) {
+						switch (which) {
+						// "Delete" is the first option in the array
+						case 0:
+							Session.getFriendManager().removeFriend(friend);
+							break;
+						default:
+							break;
+						}
+					}
+				});
+		builder.create().show();
+	}
+	
+	// Load the conversation with this friend
+	private void loadConversation(User friend) {
+		// Pass the friend so conversationActivity knows whose conversation to
+		// display.
+		// Convert to json to make it easy to pass to the object
+		String friendAsJson = Utility.toJson(friend);
+
+		Intent i = new Intent(mContext, ConversationActivity.class);
+		i.putExtra("contact", friendAsJson);
+		startActivity(i);
 	}
 	
 	@Override
